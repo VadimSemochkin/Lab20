f51f4f66662a7615324290ee7ecbf637066eb4d3
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2022-01-08 17:52:49 +0300

    Добавьте файлы проекта.

---
+++ Lab20/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab20
+{
+    class Program
+    {
+        delegate  double MyDelegate(double R);
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус окружности:");
+            double r = Convert.ToDouble(Console.ReadLine());
+            MyDelegate myDelegate = new MyDelegate(Lenth);
+            myDelegate += Square;
+            myDelegate += Volume;
+            myDelegate?.Invoke(r);
+            Console.ReadKey();
+
+        }
+
+        static double Lenth(double R)
+        {
+            double D = 2 * R * Math.PI;
+            Console.WriteLine($"Длинна окружности равна {D}");
+            return D;
+        }
+        static double Square(double R)
+        {
+            double S = R * R * Math.PI;
+            Console.WriteLine($"Площадь круга равна {S}");
+            return S;
+        }
+        static double Volume(double R)
+        {
+            double V = (4/3)*Math.Pow(R,3)*Math.PI;
+            Console.WriteLine($"Объем шара равен {V}");
+            return V;
+        }
+    }
+}
